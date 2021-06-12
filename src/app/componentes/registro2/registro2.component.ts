a1429dab74f6b6c1940e0a5ae475fea21cee6561
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-06-12 16:47:23 +0200

    Vamos a comprobar xke falla el formulario

--- src/app/componentes/registro2/registro2.component.ts
+++ src/app/componentes/registro2/registro2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-registro2',
@@ -12,15 +12,18 @@ export class Registro2Component implements OnInit {
   
   constructor( private _fb:FormBuilder ){
     this.form1 = this._fb.group({
-      NombreCompleto : ['',[Validators.required]],
-      CorreoElectronico : ['',[Validators.required,Validators.email]],
-      Contrasenia : ['',[Validators.required]],
-    })
+      NombreCompleto : ['', [Validators.required]],
+      CorreoElectronico : ['', [Validators.required]],
+      Contrasenia : ['', [Validators.required]],
+      tyc : [false, Validators.required]
+    });
   }
 
   ngOnInit(): void {
   }
 
-  avanzar(){}
+  avanzar(){
+    this.form1.valid ? console.log("VALIDO",this.form1.value) : console.log("INVALIDO",this.form1.value);
+  }
 
 }
