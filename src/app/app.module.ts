29d773b1f3c70905b48d8abc98166d5b4388e4a1
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-06-12 17:24:01 +0200

    Comenzamos el registro 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Registro1Component } from './componentes/registro1/registro1.component';
 import { Registro2Component } from './componentes/registro2/registro2.component';
+import { Registro3Component } from './componentes/registro3/registro3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Registro1Component,
-    Registro2Component
+    Registro2Component,
+    Registro3Component
   ],
   imports: [
     BrowserModule,
